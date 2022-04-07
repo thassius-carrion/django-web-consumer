daa0317aa5c5d9df960b5537e83f608314d33604
Thassius Oliveira <thassius.oliveira@programmers.com.br>
2022-04-07 09:42:17 -0300

    getProducts retornando integrado com apifast

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests # faz requisições HTTP
 
+"""
 def index(request):
     api = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/24/municipios"
     requisicao = requests.get(api)
@@ -16,6 +17,27 @@ def index(request):
 
     contexto = {
         "municipios": dicionario
+    }
+
+    return render(request, "index.html", contexto)
+"""
+
+def index(request):
+    api = "http://127.0.0.1:8000/home/products"
+    requisicaoGet = requests.get(api)
+
+    try:
+        lista = requisicaoGet.json()
+
+    except ValueError:
+        print("A resposta não chegou com o formato esperado.")
+
+    dicionario = {}
+    for indice, valor in enumerate(lista):
+        dicionario[indice] = valor
+
+    contexto = {
+        "products": dicionario
     }
 
     return render(request, "index.html", contexto)
